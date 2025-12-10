f57b20f3b616be553165a5332ea8822a05ebd092
NeyoNought47 <kaito_neyo@yahoo.com>
2025-12-09 19:26:50 -0500

    fixed

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "Step 1: Cleaning..."
+echo "Step 1: Cleaning"
 python clean_es_en.py
 
-echo "Step 2: Splitting train/dev/test..."
+echo "Step 2: Splitting train/dev/test"
 python split_es_en.py
 
-echo "Step 3: Training SentencePiece..."
+echo "Step 3: Training SentencePiece"
 python train_sentencepiece.py
 
-echo "Step 4: Encoding train/dev/test with SPM..."
+echo "Step 4: Encoding train/dev/test with SPM"
 python encode_es_en.py
 
-echo "All steps completed!"
+echo "Completed"
 
